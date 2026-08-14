0cafdf3c82689e004ee5bd76dcd331b05b55ed95
Harry-at-G <harry.wirawan@gmail.com>
2026-08-14 15:52:41 +0700

    Add recommendation engine

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,5 @@
+from services.trip_service import calculate_daily_budget, get_trip_category, get_travel_season
+
 # Function for printing the output
 def print_trip_summary(destination, country, days, budget, currency, travel_month, total_cost, travel_style):
     print("=============================")
@@ -23,7 +25,7 @@ food_cost   = float(input("Food Cost : "))
 trans_cost  = float(input("Transport Cost : "))
 misc_cost   = float(input("Misc. Cost : "))
 travel_month= input("Travel Month : ")
-travel_style= input("Travel Stryle : ")
+travel_style= input("Travel Style : ")
 
 # Calculate Costs and Compare to Budget
 total_cost = hotel_cost + food_cost + trans_cost + misc_cost
@@ -32,3 +34,43 @@ if total_cost > budget:
 
 # Using Function to print the output
 print_trip_summary(destination,country,days,budget,currency, travel_month, total_cost,travel_style)
+
+# Translate business rules into code
+if budget < 1000:
+    category = "Backpacker"
+elif budget <= 3000:
+    category = "Standard"
+else:
+    category = "Luxury"
+print(f"Category        : {category}")
+    
+# Arithmetic operators: + - * / //
+daily_budget = budget/days
+print(f"Daily Budget    : {daily_budget} {currency}/day")
+
+# A list holds multiple values
+recommended_places = [
+    "Tokyo Tower",
+    "Shibuya",
+    "Mount Fuji"
+]
+
+# Loop through the list
+print("Recommended Places:")
+for place in recommended_places:
+    print(f" - {place}")
+
+daily = calculate_daily_budget(budget,days)
+category = get_trip_category(budget)
+print(f"{category} - {daily} {currency}/day")
+
+if category == "Backpacker":
+    recommended_transportation = "Bus"
+elif category == "Standard":
+    recommended_transportation = "Train"
+else: 
+    recommended_transportation = "Flight"
+
+print(f"Recommended Transportation: {recommended_transportation}")
+season = get_travel_season(travel_month)
+print(f"Season          : {season}")
